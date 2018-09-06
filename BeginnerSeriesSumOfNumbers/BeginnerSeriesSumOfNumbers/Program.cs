bbd8a19936f447a02c0d8c2a4df0665f3354cb0c
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-06 09:34:30 +0900

    BeginnerSeriesSumOfNumbers! Add Solution

---
+++ BeginnerSeriesSumOfNumbers/BeginnerSeriesSumOfNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerSeriesSumOfNumbers
+{
+    /// <summary>
+    /// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. 
+    /// If the two numbers are equal return a or b.
+    /// Note: a and b are not ordered!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            Console.WriteLine(sum.GetSum(1, 0));
+            Console.WriteLine(sum.GetSum(1, 2));
+            Console.WriteLine(sum.GetSum(0, 1));
+            Console.WriteLine(sum.GetSum(1, 1));
+            Console.WriteLine(sum.GetSum(-1, 0));
+            Console.WriteLine(sum.GetSum(1, 0));
+            Console.WriteLine(sum.GetSum(-1, 2));
+            Console.ReadLine();
+
+        }
+    }
+    public class Sum
+    {
+        public int GetSum(int a, int b)
+        {
+            if (a == b) return a;
+            var sum = 0;
+            for (int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
