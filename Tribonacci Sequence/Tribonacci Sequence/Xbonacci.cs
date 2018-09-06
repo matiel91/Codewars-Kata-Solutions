b80a0c9004c47e31825f2a9f7a5f690a64c7d95d
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-06 09:03:47 +0900

    Tribonacci Sequence! Add Solution

---
+++ Tribonacci Sequence/Tribonacci Sequence/Xbonacci.cs
@@ -0,0 +1,43 @@
+﻿namespace Tribonacci_Sequence
+{
+    /// <summary>
+    /// Tribonacci Sequence
+    /// Well met with Fibonacci bigger brother, AKA Tribonacci.
+    /// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next.
+    /// </summary>
+    public class Xbonacci
+    {
+
+        /// <summary>
+        /// Method takes array of numbers and returning n-element array of "Tribonacci sequence" 
+        /// </summary>
+        /// <param name="signature">Input array</param>
+        /// <param name="n">Numbers of element in tribonacci sequnce</param>
+        /// <returns>Returning n-element array (double[]) of "Tribonacci sequence" </returns>
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            if (n == 0)
+            {
+                return new double[1] {0};
+            }
+            double[] resultArray = new double[n];
+            if (n>0 && n<4)
+            {
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    resultArray[i] = signature[i];
+                }
+                return resultArray;
+            }
+            resultArray[0] = signature[0];
+            resultArray[1] = signature[1];
+            resultArray[2] = signature[2];
+            for (int i = 3; i <resultArray.Length; i++)
+            {
+                resultArray[i] = resultArray[i - 3] + resultArray[i - 2] + resultArray[i - 1];
+            }
+
+            return resultArray;
+        }
+    }
+}
---
+++ Tribonacci Sequence/TribonacciSequence.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tribonacci_Sequence;
+
+namespace TribonacciSequence.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase(new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 }, new double[] { 1, 1, 1 }, 10)]
+        [TestCase(new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 }, new double[] { 0, 0, 1 }, 10)]
+        [TestCase(new double[] { 0, 1, 1, 2, 4, 7, 13, 24, 44, 81 }, new double[] { 0, 1, 1 }, 10)]
+        public void Tests(double[] expectedResult, double[] inputArray,  int n)
+        {
+            Xbonacci xbonacci = new Xbonacci();
+            Assert.AreEqual(expectedResult, xbonacci.Tribonacci(inputArray, n));
+           
+        }
+    }
+}
