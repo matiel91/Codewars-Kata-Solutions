c5e9a99d6e49d15836b89af53f40a1ffe1ca0862
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-06 09:25:29 +0900

    Are The Same! Add Solution

---
+++ Are The Same/Are The Same/Kata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Are_The_Same
+{
+    /// <summary>
+    /// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether 
+    /// the two arrays have the "same" elements, with the same multiplicities.
+    /// "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+    /// </summary>
+    class Kata
+    {
+
+        public static bool comp(int[] a, int[] b)
+        {
+
+            if (a == null|| b == null) return false;
+            var listA = a.ToList();
+            var listB = b.ToList();
+            var count = 0;
+            //A block for preliminary checking if iterate is required
+            //A 
+            if (a.Length == 0 && b.Length == 0) return true;
+            else if (a.Length == 0 || b.Length == 0) return false;            
+            var diffrence = a.Select(i => i * i).Except(b);
+            if (diffrence.Count() > 0) return false;
+            //A
+            for (int i = 0; i < listA.Count; i++)
+            {
+                count = 0;
+                for (int j = 0; j < listB.Count; j++)
+                {
+                    if(Math.Pow(listA[i], 2) == listB[j])
+                    {
+                        listB.Remove(listB[j]);
+                        count++;
+                        continue;
+                    }
+                    if (count > 0) continue;
+                }
+                //if (count == 0) return false;
+            }
+            if(listB.Count>0) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Are The Same/Are The Same/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Are_The_Same
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[0];
+            int[] b = new int[0];
+            int[] c = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] d = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            
+            Console.WriteLine(Kata.comp(a, null));
+            Console.WriteLine( Kata.comp(c, d));
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
