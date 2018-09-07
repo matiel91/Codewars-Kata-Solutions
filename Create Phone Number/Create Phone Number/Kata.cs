3da855356840aa9b926f48da75848c61f4c3e7a3
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-06 22:17:34 -0200

    Create Phone Number! Add solution

---
+++ Create Phone Number/Create Phone Number/Kata.cs
@@ -0,0 +1,18 @@
+﻿namespace Create_Phone_Number
+{
+    /// <summary>
+    /// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+    /// Format: "(123) 456-7890"
+    /// </summary>
+    public static class Kata
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            if (numbers.Length != 10) {
+                throw new System.Exception("Array lenght !=10");
+            }
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+        }
+
+    }
+}
---
+++ Create Phone Number/CreatePhoneNumber.tests/PhoneNumberCreatorTest.cs
@@ -0,0 +1,26 @@
+﻿using Create_Phone_Number;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatePhoneNumber.tests
+{
+    [TestFixture]
+    public class PhoneNumberCrteatorTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, Result = "(123) 456-7890")]
+        [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, Result = "(111) 111-1111")]
+        public string PhoneNumberCreatedProperly(int[] array)
+        {
+
+            var result = Kata.CreatePhoneNumber(array);
+            return result; //w tym wypadku kompilator asertuje sam
+
+        }
+
+    }
+}
+
