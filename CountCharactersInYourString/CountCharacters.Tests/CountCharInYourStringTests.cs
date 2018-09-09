6bd8b401bf4169e99a43b8fc6eef69c8baebc9d9
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-09 18:56:05 -0200

    Count characters in your string! Add Solution

---
+++ CountCharactersInYourString/CountCharacters.Tests/CountCharInYourStringTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountCharactersInYourString;
+namespace CountCharacters.Tests
+{
+    [TestFixture]
+    public class CountCharInYourStringTests
+    {
+        [Test]
+        public static void FixedTest_aaaa1st()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 4);
+            Assert.AreEqual(d, Kata.Count("aaaa"));
+        }
+
+        [Test]
+        public static void FixedTest_aabb1st()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 2);
+            d.Add('b', 2);
+            Assert.AreEqual(d, Kata.Count("aabb"));
+        }
+
+        [Test]
+        public static void FixedTest_aaaa2nd()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 4);
+            Assert.AreEqual(d, Kata.Count2("aaaa"));
+        }
+
+        [Test]
+        public static void FixedTest_aabb2nd()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 2);
+            d.Add('b', 2);
+            Assert.AreEqual(d, Kata.Count2("aabb"));
+        }
+    }
+}
---
+++ CountCharactersInYourString/CountCharactersInYourString/Kata.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace CountCharactersInYourString
+{
+    /// <summary>
+    /// Count characters in your string 
+    /// The main idea is to count all the occuring characters(UTF-8) in string. 
+    /// If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+    /// What if the string is empty? Then the result should be empty object literal { }
+    /// For C#: Use a Dictionary<char, int> for this kata!
+    /// </summary>
+    public class Kata
+    {
+        //1st
+        public static Dictionary<char, int> Count(string str)
+        {
+            
+            var strCharArray = str.ToCharArray();
+            var dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!dictionary.ContainsKey(strCharArray[i]))
+                {
+                    dictionary.Add(strCharArray[i], 1);
+                    continue;
+                }
+                else
+                {
+                    dictionary.TryGetValue(strCharArray[i], out int value);                   
+                    dictionary[strCharArray[i]] = ++value;
+                }
+
+            }
+            return dictionary;
+        }
+        //2nd
+        public static Dictionary<char, int> Count2(string str)
+        {            
+            var dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!dictionary.ContainsKey(str[i]))
+                {
+                    dictionary.Add(str[i], 1);
+                    continue;
+                }
+                else
+                {
+                    dictionary.TryGetValue(str[i], out int value);
+                    dictionary[str[i]] = ++value;
+                }
+
+            }
+            return dictionary;
+        }
+    }
+}
