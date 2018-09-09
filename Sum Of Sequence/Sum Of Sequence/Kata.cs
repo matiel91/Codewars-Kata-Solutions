57b151424df39c4f5ec54084d358f2a7421bc1e5
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-08 22:49:12 -0200

    Sum Of Sequence! Add Solution

---
+++ Sum Of Sequence/Sum Of Sequence/Kata.cs
@@ -0,0 +1,27 @@
+﻿namespace Sum_Of_Sequence
+{
+    /// <summary>
+    /// Your task is to make function, which returns the sum of a sequence of integers.
+    /// The sequence is defined by 3 non-negative values: begin, end, step.
+    /// </summary>
+    public static class Kata
+    {
+        /// <summary>
+        /// Function calculate the sum of a sequence of integers
+        /// </summary>
+        /// <param name="start">Begin Value</param>
+        /// <param name="end">End Value</param>
+        /// <param name="step">Step Value</param>
+        /// <returns></returns>
+        public static int SequenceSum(int start, int end, int step)
+        {
+            var sum = 0;
+            if (start > end) return 0;
+            for (int i = start; i <= end; i+=step)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Sum Of Sequence/Sum Of Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Of_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SequenceSum(2, 35, 3));
+            Console.ReadLine();
+        }
+    }
+}
