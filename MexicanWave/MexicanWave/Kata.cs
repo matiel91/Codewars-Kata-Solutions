f32b41cb577d693f33f3c7fea65e936b6febbe28
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-08 23:15:05 -0200

    Mexican Wave! Add Solution

---
+++ MexicanWave/MexicanWave/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MexicanWave
+{
+    /// <summary>
+    /// In this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+    /// You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+    /// 
+    /// Example:
+    /// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+    /// </summary>
+    public class Kata
+    {
+        public List<string> Wave(string str)
+        {
+            List<string> list = new List<string>();
+            StringBuilder sB = new StringBuilder(str);
+            var strArray = str.ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strArray[i] == ' ') continue;
+                strArray[i] = Convert.ToChar(strArray[i].ToString().ToUpper());
+                list.Add(new string(strArray));
+                strArray[i] = Convert.ToChar(strArray[i].ToString().ToLower());
+
+            }
+
+            return list;
+        }
+    }
+}
---
+++ MexicanWave/MexicanWave/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MexicanWave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+            List<string> list = kata.Wave("example");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
