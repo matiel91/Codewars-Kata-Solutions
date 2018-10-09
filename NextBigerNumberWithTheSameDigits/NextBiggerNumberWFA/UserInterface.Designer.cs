e6dc6b774743d0d18978fb22e13ac29bee8b8cbd
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-10-09 08:17:42 -0200

    Next bigger number with the same digits! Basic GUI added

---
+++ NextBigerNumberWithTheSameDigits/NextBiggerNumberWFA/UserInterface.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace NextBiggerNumberWFA
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultTextLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.enterYourNumberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.resultLabel.Location = new System.Drawing.Point(147, 301);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(106, 42);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.Text = "........";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultTextLabel
+            // 
+            this.resultTextLabel.AutoSize = true;
+            this.resultTextLabel.Font = new System.Drawing.Font("Sitka Display", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextLabel.Location = new System.Drawing.Point(60, 196);
+            this.resultTextLabel.Name = "resultTextLabel";
+            this.resultTextLabel.Size = new System.Drawing.Size(319, 84);
+            this.resultTextLabel.TabIndex = 1;
+            this.resultTextLabel.Text = "The next bigger number \r\nwith same digit is: ";
+            this.resultTextLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Sitka Display", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inputTextBox.Location = new System.Drawing.Point(67, 72);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(273, 48);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Sitka Display", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.calculateButton.Location = new System.Drawing.Point(137, 138);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(144, 46);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "CALCULATE";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // enterYourNumberLabel
+            // 
+            this.enterYourNumberLabel.AutoSize = true;
+            this.enterYourNumberLabel.Font = new System.Drawing.Font("Sitka Display", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.enterYourNumberLabel.Location = new System.Drawing.Point(59, 9);
+            this.enterYourNumberLabel.Name = "enterYourNumberLabel";
+            this.enterYourNumberLabel.Size = new System.Drawing.Size(281, 47);
+            this.enterYourNumberLabel.TabIndex = 4;
+            this.enterYourNumberLabel.Text = "Enter your number";
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 398);
+            this.Controls.Add(this.enterYourNumberLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.resultTextLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Name = "UserInterface";
+            this.Text = "Next Bigger Number With The Same Digit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label resultTextLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label enterYourNumberLabel;
+    }
+}
---
+++ NextBigerNumberWithTheSameDigits/NextBiggerNumberWFA/UserInterface.cs
@@ -0,0 +1,29 @@
+﻿using NextBigerNumberWithTheSameDigits.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NextBiggerNumberWFA
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            long input;
+            resultLabel.Text = long.TryParse(inputTextBox.Text, out input) ? 
+                Kata.NextBiggerNumber(input).ToString() : "Please Enter a number";
+            //resultLabel.Text = Kata.NextBiggerNumber(long.Parse(inputTextBox.Text)).ToString();
+        }
+    }
+}
